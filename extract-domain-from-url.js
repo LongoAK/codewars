5410a358a23b8509cb6e0180ec6b06039ae60617
LongoAK <joe.longo@jjl-enterprises.com>
2021-11-16 21:50:03 -0900

    in progress

--- extract-domain-from-url.js
+++ extract-domain-from-url.js
@@ -1,8 +1,13 @@
 function domainName(url) {
-  //your code here
+  const re = /((http|https)|(www))(:|.)/;
+  if (url.split("")[0] !== "h") temp = "https://" + url;
+  let newUrl = new URL(temp).hostname.split(".");
+  return newUrl.length == 2 ? newUrl[0] : newUrl[1];
+  console.log(new URL(temp));
+  return new URL(newUrl).hostname.split(".");
 }
 
-console.log(domainName("http://google.com"), "google");
-console.log(domainName("http://google.co.jp"), "google");
-console.log(domainName("www.xakep.ru"), "xakep");
-console.log(domainName("https://youtube.com"), "youtube");
+// console.log(domainName("http://google.com"), "google");
+// console.log(domainName("http://google.co.jp"), "google");
+// console.log(domainName("www.xakep.ru"), "xakep");
+// console.log(domainName("https://youtube.com"), "youtube");
